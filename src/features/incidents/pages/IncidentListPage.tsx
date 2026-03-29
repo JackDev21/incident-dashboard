4c1918108886e80f6c72551a6beac1b43b692635
Jose A. Cantó (Jack) <jose.a.c.lopez@gmail.com>
2026-03-29 18:44:50 +0200

    refactor(incidents): usar barrel export en IncidentCard

--- src/features/incidents/pages/IncidentListPage.tsx
+++ src/features/incidents/pages/IncidentListPage.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 
 import { getIncidents } from "@/features/incidents/services/incidents.service"
 import type { Incident } from "@/features/incidents/types/incident.types"
-import { IncidentCard } from "@/features/incidents/components/IncidentCard/IncidentCard"
+import { IncidentCard } from "../components/IncidentCard"
 
 export const IncidentListPage = () => {
   const [incidents, setIncidents] = useState<Incident[]>([])
